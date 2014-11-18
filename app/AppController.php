fe048252aebe858209a702ec7258c38895523e18
Patrik Fuhrmann <fuhrmann.patrik@gmail.com>
2014-11-18 01:51:08 +0000

    Routing incorporated

---
+++ app/AppController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace COMP1687\CW;
+
+class AppController
+{
+    //
+    /**
+     * Level 1 : Account creation : 18 marks
+     * HTTP GET /registration
+     */
+    public function getRegistration()
+    {
+        echo 'here';
+    }
+
+} 
--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
-echo 'test';
+
+// Composer bootstrap
+require 'vendor/autoload.php';
+
+// Router bootstrap
+$router = new Phroute\RouteCollector();
+$router->controller('/', 'COMP1687\CW\AppController');
+$dispatcher = new Phroute\Dispatcher($router);
+$response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+// Print response from router
+echo $response;
