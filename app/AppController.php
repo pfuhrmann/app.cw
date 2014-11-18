262612a9d7db54366124d71eae398fc848d30a64
Patrik Fuhrmann <fuhrmann.patrik@gmail.com>
2014-11-18 03:15:42 +0000

    Routing fix + integrating Twig

--- app/AppController.php
+++ app/AppController.php
@@ -2,16 +2,49 @@
 
 namespace COMP1687\CW;
 
+use Twig_Environment;
+
 class AppController
 {
-    //
+    /**
+     * @var Twig_Environment
+     */
+    protected $twig;
+
+    /**
+     * @param Twig_Environment $twig
+     */
+    public function __construct(Twig_Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * Index page
+     * URI: /
+     */
+    public function getIndex()
+    {
+        echo $this->render('index.html', []);
+    }
+
     /**
      * Level 1 : Account creation : 18 marks
-     * HTTP GET /registration
+     * URI: registration
      */
     public function getRegistration()
     {
-        echo 'here';
+        echo $this->render('registration.html', []);
     }
 
-} 
+    /**
+     * Render Twig template
+     *
+     * @param $template
+     */
+    private function render($template, $options)
+    {
+        $template = $this->twig->loadTemplate($template);
+        echo $template->render($options);
+    }
+}
--- index.php
+++ index.php
@@ -3,11 +3,17 @@
 // Composer bootstrap
 require 'vendor/autoload.php';
 
+// Bootstrap Twig
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
+
 // Router bootstrap
 $router = new Phroute\RouteCollector();
-$router->controller('/', 'COMP1687\CW\AppController');
+$router->controller('/', new COMP1687\CW\AppController($twig));
 $dispatcher = new Phroute\Dispatcher($router);
-$response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+// This is hack for stuweb webserver
+$uri = (!empty($_GET['uri'])) ? $_GET['uri'] : '/';
+$response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($uri, PHP_URL_PATH));
 
 // Print response from router
 echo $response;
