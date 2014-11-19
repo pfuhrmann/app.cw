8e53a28d78c1dc74eb584e24ae98cfe7db618bc9
Patrik Fuhrmann <fuhrmann.patrik@gmail.com>
2014-11-19 00:11:05 +0000

    Reg form validation implemented

--- app/AppController.php
+++ app/AppController.php
@@ -3,6 +3,8 @@
 namespace COMP1687\CW;
 
 use Gregwar\Captcha\CaptchaBuilder;
+use PDO;
+use Respect\Validation\Validator;
 use Twig_Environment;
 
 class AppController
@@ -13,11 +15,17 @@ class AppController
     protected $twig;
 
     /**
+     * @var PDO
+     */
+    protected $db;
+
+    /**
      * @param Twig_Environment $twig
      */
     public function __construct(Twig_Environment $twig)
     {
         $this->twig = $twig;
+        $this->db = DatabaseManager::getInstance();
     }
 
     /**
@@ -35,10 +43,7 @@ class AppController
      */
     public function getRegistration()
     {
-        // Generate captcha
-        $builder = new CaptchaBuilder;
-        $builder->build();
-
+        $builder = $this->generateCaptcha();
         echo $this->render('registration.html', [
             'captcha' => $builder->inline()
         ]);
@@ -51,8 +56,73 @@ class AppController
     public function postRegistration()
     {
         $formData = $_POST;
+        $errors = [];
 
-        echo $this->render('registration.html', []);
+        // Validate username
+        $usernameValidator = Validator::alnum()->noWhitespace()->length(3, 15)->notEmpty()->uniqueUser();
+        try {
+            $usernameValidator->assert($formData['username']);
+        } catch(\InvalidArgumentException $e) {
+            $errors['username'] = array_filter($e->findMessages([
+                'alnum'        => '<strong>Username</strong> must contain only letters and digits',
+                'length'       => '<strong>Username</strong> must be between 3 and 15 characters',
+                'noWhitespace' => '<strong>Username</strong> cannot contain spaces',
+                'notEmpty'     => '<strong>Username</strong> cannot be empty',
+                'uniqueUser'   => 'This <strong>username</strong> is taken, please choose another one',
+            ]));
+        }
+
+        // Validate password
+        $passwordValidator = Validator::length(6, 20)->notEmpty();
+
+        try {
+            $passwordValidator->assert($formData['password']);
+        } catch(\InvalidArgumentException $e) {
+            $errors['password'] = array_filter($e->findMessages([
+                'notEmpty' => '<strong>Password</strong> cannot be empty',
+                'length'   => '<strong>Password</strong> length must be between 6 and 20 characters'
+            ]));
+        }
+
+        // Validate email
+        $emailValidator = Validator::email()->notEmpty();
+        try {
+            $emailValidator->assert($formData['email']);
+        } catch(\InvalidArgumentException $e) {
+            $errors['email'] = array_filter($e->findMessages([
+                'notEmpty' => '<strong>Email</strong> cannot be empty',
+                'email'    => '<strong>Email</strong> is not valid',
+            ]));
+        }
+
+        /*// Validate captcha
+        $captchaValidator = Validator::equals($_SESSION['phrase'])->notEmpty();
+        try {
+            $captchaValidator->assert($formData['captcha']);
+        } catch(\InvalidArgumentException $e) {
+            $errors['captcha'] = array_filter($e->findMessages([
+                'notEmpty' => '<strong>CAPTCHA</strong> cannot be empty',
+                'equals'   => 'Incorrect <strong>CAPTCHA</strong>, please try again',
+            ]));
+        }*/
+
+
+        // We get errors so display reg form again
+        if (!empty($errors)) {
+            $builder = $this->generateCaptcha();
+
+            return $this->render('registration.html', [
+                'input'     => $formData,
+                'errorsAll' => $errors,
+                'captcha'   => $builder->inline(),
+            ]);
+        }
+
+        // All good, create account
+        $stmt = $this->db->prepare(" INSERT INTO user (username, password, email) VALUES (?, ?, ?)");
+        var_dump($stmt->execute([
+            $formData['username'], $formData['password'], $formData['email']
+        ]));
     }
 
     /**
@@ -73,5 +143,21 @@ class AppController
     {
         $template = $this->twig->loadTemplate($template.'.twig');
         echo $template->render($options);
+    }
+
+    /**
+     * Generate new captcha
+     *
+     * @return CaptchaBuilder
+     */
+    private function generateCaptcha()
+    {
+        // Generate captcha
+        $builder = new CaptchaBuilder;
+        $builder->build();
+        // Store phrase to use later in validation
+        $_SESSION['phrase'] = $builder->getPhrase();
+
+        return $builder;
     }
 }
---
+++ validation/Rules/UniqueUser.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Respect\Validation\Rules;
+
+use COMP1687\CW\DatabaseManager;
+use PDO;
+
+/**
+ * Check if username from input is
+ *  unique within our db
+ */
+class UniqueUser extends AbstractRule
+{
+    /**
+     * @var PDO
+     */
+    protected $db;
+
+    public function __construct()
+    {
+        $this->db = DatabaseManager::getInstance();
+    }
+
+    public function validate($input)
+    {
+        $stmt = $this->db->prepare("SELECT count('id') AS count FROM accounts WHERE username=?");
+        $stmt->execute([$input]);
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return ($row['count'] === "0");
+    }
+}
