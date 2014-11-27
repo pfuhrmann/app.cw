6caaabd884c60a12ab3454e3097837275cd791b6
Patrik Fuhrmann <fuhrmann.patrik@gmail.com>
2014-11-27 23:24:12 +0000

    Progress on pagination

--- app/Controllers/SearchController.php
+++ app/Controllers/SearchController.php
@@ -4,6 +4,7 @@ namespace COMP1687\CW\Controllers;
 
 use PDO;
 use Respect\Validation\Validator;
+use Kilte\Pagination\Pagination;
 
 /**
  * Handles Service search function
@@ -26,6 +27,8 @@ class SearchController extends BaseController
     public function postSearch()
     {
         $formData = $_POST;
+        $currentPage = (isset($_GET['page'])) ? $_GET['page'] : 1;
+        $itemsPerPage = 5;
 
         // Get search type
         if (isset($formData['by-type'])) {
@@ -40,29 +43,68 @@ class SearchController extends BaseController
         $formData['searchType'] = $searchType;
         $imageOnly = (isset($formData['imageonly']) && $formData['imageonly'] === 'on');
 
+        //
         // Search by sitter type
+        //
         if ($searchType === 'type') {
             $formData['postcode'] = '';
 
             // Search for only posts with images
             if ($imageOnly) {
+                // Prepare pagination
+                $query = "SELECT COUNT(s.id) AS count FROM service s
+                             INNER JOIN account a ON s.account_id = a.id
+                             INNER JOIN image i ON a.id = i.account_id
+                           WHERE s.type=?
+                             AND i.defaultimg = 1";
+                $stmt = $this->db->prepare($query);
+                $stmt->execute([$formData['type']]);
+                $totalItems = $stmt->fetch(PDO::FETCH_ASSOC)['count'];
+                $pagination = new Pagination($totalItems, $currentPage, $itemsPerPage);
+                $limit = $pagination->limit();
+                $offset = $pagination->offset();
+
+                // Get posts
                 $query = "SELECT s.*, i.name, i.alt FROM service s
                             INNER JOIN account a ON s.account_id = a.id
                             INNER JOIN image i ON a.id = i.account_id
-                          WHERE s.type=?
-                            AND i.defaultimg = 1";
+                          WHERE s.type=:type
+                            AND i.defaultimg = 1
+                          LIMIT :limit
+                          OFFSET :offset";
                 $stmt = $this->db->prepare($query);
             } else {
-                $query = "SELECT s.*, i.name, i.alt FROM service s
+                // Prepare pagination
+                $query = "SELECT COUNT(s.id) AS count FROM service s
                             INNER JOIN account a ON s.account_id = a.id
                             LEFT JOIN image i ON a.id = i.account_id
                           WHERE s.type=?
                             AND IF ((SELECT COUNT(*) FROM image WHERE account_id = i.account_id), i.defaultimg = 1, 1=1)";
                 $stmt = $this->db->prepare($query);
+                $stmt->execute([$formData['type']]);
+                $totalItems = $stmt->fetch(PDO::FETCH_ASSOC)['count'];
+                $pagination = new Pagination($totalItems, $currentPage, $itemsPerPage);
+                $limit = $pagination->limit();
+                $offset = $pagination->offset();
+
+                // Get posts
+                $query = "SELECT s.*, i.name, i.alt FROM service s
+                            INNER JOIN account a ON s.account_id = a.id
+                            LEFT JOIN image i ON a.id = i.account_id
+                          WHERE s.type=:type
+                            AND IF ((SELECT COUNT(*) FROM image WHERE account_id = i.account_id), i.defaultimg = 1, 1=1)
+                          LIMIT :limit
+                          OFFSET :offset";
+                $stmt = $this->db->prepare($query);
             }
-            $stmt->execute([$formData['type']]);
+            $stmt->bindParam(':type', $formData['type'], PDO::PARAM_STR);
+            $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
+            $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
+            $stmt->execute();
             $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        //
         // Search by postcode
+        //
         } else if ($searchType === 'postcode') {
             $formData['type'] = '';
 
@@ -89,21 +131,56 @@ class SearchController extends BaseController
 
             // Search for only posts with images
             if ($imageOnly) {
+                // Prepare pagination
+                $query = "SELECT COUNT(s.id) AS count FROM service s
+                             INNER JOIN account a ON s.account_id = a.id
+                             INNER JOIN image i ON a.id = i.account_id
+                           WHERE s.postcode LIKE ?
+                             AND i.defaultimg = 1";
+                $stmt = $this->db->prepare($query);
+                $stmt->execute(['%'.$formData['postcode'].'%']);
+                $totalItems = $stmt->fetch(PDO::FETCH_ASSOC)['count'];
+                $pagination = new Pagination($totalItems, $currentPage, $itemsPerPage);
+                $limit = $pagination->limit();
+                $offset = $pagination->offset();
+
+                // Get posts
                 $query = "SELECT s.*, i.name, i.alt FROM service s
                             INNER JOIN account a ON s.account_id = a.id
                             INNER JOIN image i ON a.id = i.account_id
-                          WHERE s.postcode LIKE ?
-                            AND i.defaultimg = 1";
+                          WHERE s.postcode LIKE :postcode
+                            AND i.defaultimg = 1
+                          LIMIT :limit
+                          OFFSET :offset";
                 $stmt = $this->db->prepare($query);
             } else {
+                // Prepare pagination
+                $query = "SELECT COUNT(s.id) AS count FROM service s
+                             INNER JOIN account a ON s.account_id = a.id
+                             LEFT JOIN image i ON a.id = i.account_id
+                           WHERE s.postcode LIKE ?
+                              AND IF ((SELECT COUNT(*) FROM image WHERE account_id = i.account_id), i.defaultimg = 1, 1=1)";
+                $stmt = $this->db->prepare($query);
+                $stmt->execute(['%'.$formData['postcode'].'%']);
+                $totalItems = $stmt->fetch(PDO::FETCH_ASSOC)['count'];
+                $pagination = new Pagination($totalItems, $currentPage, $itemsPerPage);
+                $limit = $pagination->limit();
+                $offset = $pagination->offset();
+
                 $query = "SELECT s.*, i.name, i.alt FROM service s
                             INNER JOIN account a ON s.account_id = a.id
                             LEFT JOIN image i ON a.id = i.account_id
-                          WHERE s.postcode LIKE ?
-                            AND IF ((SELECT COUNT(*) FROM image WHERE account_id = i.account_id), i.defaultimg = 1, 1=1)";
+                          WHERE s.postcode LIKE :postcode
+                            AND IF ((SELECT COUNT(*) FROM image WHERE account_id = i.account_id), i.defaultimg = 1, 1=1)
+                          LIMIT :limit
+                          OFFSET :offset";
                 $stmt = $this->db->prepare($query);
             }
-            $stmt->execute(['%'.$formData['postcode'].'%']);
+            $searchParam = '%'.$formData['postcode'].'%';
+            $stmt->bindParam(':postcode', $searchParam, PDO::PARAM_STR);
+            $stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
+            $stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
+            $stmt->execute();
             $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
 
